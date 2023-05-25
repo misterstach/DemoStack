1feaf8d9b536d3f07bab8960f284a12b8087de45
MisterSatch <jacky.revet@gmail.com>
2023-05-25 21:29:53 +0200

    optimisation des imports

--- Backend/src/main/java/com/example/service/UserService.java
+++ Backend/src/main/java/com/example/service/UserService.java
@@ -2,16 +2,11 @@ package com.example.service;
 
 import com.example.model.User;
 import com.example.repository.UserRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class UserService implements UserDetailsService
+public class UserService
 {
     private final UserRepository userRepository;
 
@@ -23,14 +18,5 @@ public class UserService implements UserDetailsService
     public List<User> getAllUsers()
     {
         return userRepository.findAll();
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
-    {
-        User user = userRepository.findById(username)
-                .orElseThrow(() -> new UsernameNotFoundException("No user found with username: " + username));
-
-        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), new ArrayList<>());
     }
 }
